f35023efb38febda62008679507891532daeab43
imad <imadamroug89@outlook.com>
2025-11-06 14:25:00 +0100

    Initial Commit

---
+++ srvmgnt/srvmgntApp.swift
@@ -0,0 +1,17 @@
+//
+//  srvmgntApp.swift
+//  srvmgnt
+//
+//  Created by Imad on 06/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct srvmgntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
